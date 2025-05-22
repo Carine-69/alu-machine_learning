98a324878a8bd93545cbc050040f5265201ec712
Carine-69 <umugabecarine@gmail.com>
2025-05-22 15:20:39 +0200

    flipping

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,19 +4,25 @@
 This module provides a function to compute the transpose of a 2D matrix
 without using any external libraries.
 
-A transpose operation converts all rows of a matrix into columns and vice versa.
+A transpose operation converts all rows of
+a matrix into columns and vice versa.
 """
+
 
 def matrix_transpose(matrix):
     """
     Transposes a given 2D matrix (list of lists).
 
-    The transpose of a matrix is obtained by switching its rows with its columns.
-    For a matrix of size m x n, the result will be a matrix of size n x m.
+    The transpose of a matrix is obtained by
+    switching its rows with its columns.
+    For a matrix of size m x n, the result
+    will be a matrix of size n x m.
 
     Parameters:
-    matrix (list of list of numbers): The matrix to transpose. It should be
-    rectangular (i.e., all rows should have the same number of elements).
+    matrix (list of list of numbers): The
+    matrix to transpose. It should be
+    rectangular (i.e., all rows should 
+    have the same number of elements).
 
     Returns:
     list of list of numbers: A new transposed matrix.
@@ -25,6 +31,8 @@ def matrix_transpose(matrix):
     >>> matrix_transpose([[1, 2, 3], [4, 5, 6]])
     [[1, 4], [2, 5], [3, 6]]
     """
+
+
     transposed = []
     for i in range(len(matrix[0])):  # loop over columns
         row = []
