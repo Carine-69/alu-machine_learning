f0cf2c7f1bd8e27f24b5c44eb74b06766ca5ed21
Carine-69 <umugabecarine@gmail.com>
2025-05-22 16:12:10 +0200

    adding vectors

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -6,5 +6,5 @@
 
 def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
-        return None , 
-    return [x+y for x,y in zip(arr1, arr2)] 
+        return None,
+    return [x+y for x, y in zip(arr1, arr2)]
