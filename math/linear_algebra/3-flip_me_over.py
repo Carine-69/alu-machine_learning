20493b302a2de9b5d3252c3f7ef84810cb8d3287
Carine-69 <umugabecarine@gmail.com>
2025-05-22 15:02:25 +0200

    flipping

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 def transpose(matrix):
-  transposed = []
+  matrix_transpose = []
   for i in range (len(matrix[0])):
     row = []
     for a in range(len(matrix)):
 
       row.append(matrix[a][i])
-      transposed.append(row)
-  return(transposed)
+      matrix_transpose.append(row)
+  return(matrix_transpose)
