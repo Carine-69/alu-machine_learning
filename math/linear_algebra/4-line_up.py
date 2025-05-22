ac52da2885f087d7e6189de223f96f6b846b70e3
Carine-69 <umugabecarine@gmail.com>
2025-05-22 15:56:57 +0200

    adding vectors

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -6,9 +6,4 @@
 def add_arrays(arr1, arr2):
     if len(arr1) != len(arr2):
         raise ValueError("arrays must have same length")
-    return [x+y for x,y in zip(arr1, arr2)]
-
-arr1 = [1,2,3,4]
-arr2 = [5,6,7,8]
-print(add_arrays(arr1, arr2))
-        
+    return [x+y for x,y in zip(arr1, arr2)] 
