51e7f9a27fad2d917840248d6c09d63c21e35471
Carine-69 <umugabecarine@gmail.com>
2025-05-22 17:08:09 +0200

    adding arrays

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -11,9 +11,3 @@ def cat_arrays(arr1, arr2):
     """
     concatenated_arr = arr1 + arr2
     return concatenated_arr
-
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
