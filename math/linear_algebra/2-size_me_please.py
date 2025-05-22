f8c6e48554f3bb935534746f4fe301220a5f7793
Carine-69 <umugabecarine@gmail.com>
2025-05-22 14:14:19 +0200

    shape of matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """
+
 module for returning the shape 
 or dimension of any given matrix
 """
+
+
 def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
