6fae4875bcf98cb0aaf2c5a869e35403a37bbf9e
Carine-69 <umugabecarine@gmail.com>
2025-05-22 14:03:56 +0200

    shape of matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-  shape = []
-  while isinstance(matrix, list):
-    shape.append(len(matrix))
-    matrix = matrix[0]if matrix else []
-  return shape
+      shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]if matrix else []
+    return shape
