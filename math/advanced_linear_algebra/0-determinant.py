17c3879ae3fc7953a6053581881bf45ffda5a186
Carine-69 <umugabecarine@gmail.com>
2025-05-30 14:36:47 +0200

    0 work

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,10 @@
+import numpy as np
+def inverse(matrix):
+   if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+   if any(len(row) != len(matrix) for row in matrix):
+    raise TypeError("matrix must be a square matrix")
+    if matrix == [[]]:
+      return(1)
+
+   return(np.linalg.det(np.array(matrix)))
