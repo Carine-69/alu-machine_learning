0b0e41c4bd3d4e9c562fbda8d45901b41c7ca6f2
Carine-69 <umugabecarine@gmail.com>
2026-02-23 21:52:02 +0200

    supervised learned

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-"""PCA on a dataset"""
+"""PCA v2 module"""
 import numpy as np
 
 
 def pca(X, ndim):
-    """Performs PCA on a dataset.
+    """Performs PCA on a dataset reducing to ndim dimensions.
 
     Args:
-        X: numpy.ndarray of shape (n, d) containing the dataset
-        ndim: new dimensionality of the transformed X
+        X: numpy.ndarray of shape (n, d)
+        ndim: new dimensionality
 
     Returns:
-        T: numpy.ndarray of shape (n, ndim) containing the transformed X
+        T: numpy.ndarray of shape (n, ndim)
     """
-    X_mean = X - np.mean(X, axis=0)
-    _, _, Vt = np.linalg.svd(X_mean)
-    W = Vt[:ndim].T
-    T = X_mean @ W
-
-    return T
+    X_m = X - np.mean(X, axis=0)
+    U, s, Vh = np.linalg.svd(X_m)
+    W = Vh[:ndim].T
+    return np.matmul(X_m, W)
