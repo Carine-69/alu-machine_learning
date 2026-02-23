c2282d4e23b54362ea14e815b38cd4244968af94
Carine-69 <umugabecarine@gmail.com>
2026-02-23 21:12:15 +0200

    supervised learned

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""PCA module"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Performs PCA on a dataset maintaining var fraction of variance.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) with zero mean dimensions
+        var: fraction of variance to maintain
+
+    Returns:
+        W: weights matrix of shape (d, nd)
+    """
+    U, s, Vh = np.linalg.svd(X)
+    cumvar = np.cumsum(s ** 2) / np.sum(s ** 2)
+    nd = np.argmax(cumvar >= var) + 1
+    return Vh[:nd].T
