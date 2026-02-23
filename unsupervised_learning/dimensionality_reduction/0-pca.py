8814deeb7a19224c8d37b5a5e979e8e3c501a171
UMUGABEKAZI <155974482+Carine-69@users.noreply.github.com>
2026-02-23 21:54:43 +0200

    Update 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-"""PCA on a dataset"""
+"""PCA module"""
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """Performs PCA on a dataset.
+    """Performs PCA on a dataset maintaining var fraction of variance.
 
     Args:
-        X: numpy.ndarray of shape (n, d) containing the dataset
+        X: numpy.ndarray of shape (n, d) with zero mean dimensions
         var: fraction of variance to maintain
 
     Returns:
-        W: numpy.ndarray of shape (d, nd) containing the weights matrix
+        W: weights matrix, shape (d, nd)
     """
-    _, s, Vt = np.linalg.svd(X)
-    cumvar = np.cumsum(s) / np.sum(s)
+    U, s, Vh = np.linalg.svd(X)
+    cumvar = np.cumsum(s ** 2) / np.sum(s ** 2)
     nd = np.argmax(cumvar >= var) + 1
-    W = Vt[:nd].T
-
-    return W
-    
+    return Vh[:nd].T
